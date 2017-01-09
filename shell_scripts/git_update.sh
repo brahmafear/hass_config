564760cf70bb71435ef93c006d98da73fb5e20d6
Brahma Fear <brahmafear@gmail.com>
2017-01-09 14:08:22 -0500

    Fixed yweather and fixed gitupdate.sh

--- shell_scripts/git_update.sh
+++ shell_scripts/git_update.sh
@@ -3,11 +3,11 @@
 cd /srv/hass/cfg
 source /srv/hass/bin/activate
 /srv/hass/bin/hass -c /srv/hass/cfg --script check_config
+echo -n "Enter the Description for the Change: " [Minor Update]
+read CHANGE_MSG
 
 git add .
 git status
-echo -n "Enter the Description for the Change: " [Minor Update]
-read CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
 git push origin master
 
